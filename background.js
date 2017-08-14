2a73697f7be36655c23ac13ab274ab225c3bd5b9
CheeseBoyd <ajlanot@gmail.com>
2017-08-14 22:44:38 +0800

    project re-write and refactor

--- background.js
+++ background.js
@@ -1,50 +1,24 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log("browser clicked");
-})
+function createAlarm() {
+  var now = new Date();
+  var day = now.getDate();
+  if (now.getHours() >= 24) {
+      // 12 AM already passed
+      day += 1;
+  }
+  // '+' casts the date to a number, like [object Date].getTime();
+  var timestamp = +new Date(now.getFullYear(), now.getMonth(), day, 3, 0, 0, 0);
+  //                        YYYY               MM              DD  HH MM SS MS
 
-// Clock and stopwatch here
+  // Create
+  chrome.alarms.create('12AMyet', {
+      when: timestamp
+  });
+}
 
-// gets the url of the active tab
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    console.log(tabs[0].url);
+// Listen
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (alarm.name === '12AMyet') {
+      // Whatever you want
+  }
 });
-
-// -- test time
-var sec = 0
-var minutes = 0;
-var hours = 0;
-function printStopWatch(seconds){
-	if(seconds>=60){
-		sec = 0;
-		minutes++;
-	}if(minutes>=60){
-		minutes = 0;
-		hours++
-	}
-
-	console.log(hours+":"+minutes+":"+sec)
-}
-
-
-
-// Adjusted clock
-
-var interval = 1000; // ms
-var expected = Date.now() + interval;
-// setTimeout(step, interval);
-function step() {
-    var dt = Date.now() - expected; // the drift (positive for overshooting)
-    if (dt > interval) {
-        // something really bad happened. Maybe the browser (tab) was inactive?
-        // possibly special handling to avoid futile "catch up" run
-    }
-	 // do what is to be done
-	expected += interval;
-	printStopWatch(sec++)
-	setTimeout(step, Math.max(0, interval - dt)); // take into account drift
-
-}
-
-// step()
-
-console.log("confirm bacground.js")
+createAlarm();
--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
-
-
-
+var callback = function () {
+    document.getElementById("#status").innerHTML = "History Cleared for the day"
+  };
+  
+  var millisecondsPerDay = 1000 * 60 * 60 * 24;
+  var oneDayAgo = (new Date()).getTime() - millisecondsPerDay;
+  chrome.browsingData.remove({
+    "since": oneDayAgo
+  }, {
+    "appcache": true,
+    "cache": true,
+    "cookies": true,
+    "downloads": true,
+    "fileSystems": true,
+    "formData": true,
+    "history": true,
+    "indexedDB": true,
+    "localStorage": true,
+    "pluginData": true,
+    "passwords": true,
+    "webSQL": true
+  }, callback);
